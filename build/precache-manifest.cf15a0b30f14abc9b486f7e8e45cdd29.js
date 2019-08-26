dfa1d235c6cd1c0a9cea590e7a3e5e31b3c8590f
billgong(巩志彬) <billgong@wesure.cn>
2019-08-26 11:06:18 +0800

    add build files

---
+++ build/precache-manifest.cf15a0b30f14abc9b486f7e8e45cdd29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c6e458f610c73e3053b4aa6a47b554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04a438d1e3d1c35a5db2",
+    "url": "/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "1bb45be857478bb850eb",
+    "url": "/static/css/main.1c59c9b4.chunk.css"
+  },
+  {
+    "revision": "04a438d1e3d1c35a5db2",
+    "url": "/static/js/2.dd063028.chunk.js"
+  },
+  {
+    "revision": "1bb45be857478bb850eb",
+    "url": "/static/js/main.a9a8f43b.chunk.js"
+  },
+  {
+    "revision": "b656b94c3a7f49d0a7cd",
+    "url": "/static/js/runtime~main.e065912b.js"
+  }
+]);
